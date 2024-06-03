9c1ab43e314bdc7cd00827cdb6fefbbedaa7416b
jakubhovadik <jakub.hovadik15@gmail.com>
2024-06-03 17:37:28 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let images = ["scorpion.png", "subzero.png", "smoke.png", "raiden.png", "group.png", "kenschi.png" ];
-let currentIndex = 0;
-
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById("slideshow").src = images[currentIndex];
-}
-
-setInterval(changeImage, 5000);
